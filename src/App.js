a46d20db4ae6b6dbf1e95f4c9f90a42f5ffbc281
jnsrjl <jnsrjl@gmail.com>
2017-03-10 13:42:42 +0000

    Add checkbox which registers onMouseDown event listener

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import React, { Component } from 'react';
 import Paper from 'material-ui/Paper';
-import RaisedButton from 'material-ui/RaisedButton';
+import Checkbox from 'material-ui/Checkbox';
 
 const style = {
+  paper: {
     width: 300,
     height: 300,
     margin: 20,
     padding: 20,
+  },
 };
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checked: false,
+    };
+    this.onMouseDown = this.onMouseDown.bind(this);
+  }
+
   render() {
     return (
-      <div>
-        <Paper style={ style }>
-          <RaisedButton label="Hello, World!" />
-        </Paper>
-      </div>
+      <Paper style={ style.paper } id="paper">
+        <Checkbox
+          checked={this.state.checked}
+          label="onMouseDown"
+          onCheck={ this.toggleOnMouseDown }
+        />
+      </Paper>
     );
+  }
+
+  toggleOnMouseDown = () => {
+    if (this.state.checked) {
+      document.getElementById("paper").removeEventListener('mousedown', this.onMouseDown, false);
+      console.log("Unregistered");
+      this.setState({ checked: false });
+    }
+    else {
+      document.getElementById("paper").addEventListener('mousedown', this.onMouseDown, false);
+      console.log("Registered");
+      this.setState({ checked: true });
+    }
+  }
+
+  onMouseDown = () => {
+    console.log("Mouse Click!");
   }
 }
 
