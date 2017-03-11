47dc795d92a7f0e03dbded5ef1e3c70aee47fcf9
jnsrjl <jnsrjl@gmail.com>
2017-03-11 14:00:25 +0000

    Update text based on onMouseDown-listener

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Paper from 'material-ui/Paper';
 import Checkbox from 'material-ui/Checkbox';
+// import List from 'material-ui/List';
+// import ListItem from 'material-ui/List';
+// import Subheader from 'material-ui/Subheader';
 
 const style = {
   paper: {
@@ -17,17 +20,19 @@ class App extends Component {
     this.state = {
       checked: false,
     };
-    this.onMouseDown = this.onMouseDown.bind(this);
   }
 
   render() {
     return (
-      <Paper style={ style.paper } id="paper">
+      <Paper style={ style.paper } id="paper" zDepth={5}>
         <Checkbox
           checked={this.state.checked}
           label="onMouseDown"
           onCheck={ this.toggleOnMouseDown }
         />
+        <ul>
+          <li id="onMouseDownText">Not listening onMouseDown</li>
+        </ul>
       </Paper>
     );
   }
@@ -45,8 +50,18 @@ class App extends Component {
     }
   }
 
-  onMouseDown = () => {
-    console.log("Mouse Click!");
+  onMouseDown = (event) => {
+    document.getElementById("onMouseDownText").innerHTML = "Mouse x: " + event.clientX + " Mouse y: " + event.clientY;
+  }
+
+  // Prevent memory leak
+  componentWillUnmount() {
+    // Check if listener is registered
+    if (this.state.checked) {
+      document.getElementById("paper").removeEventListener('mousedown', this.onMouseDown, false);
+      console.log("Unregistered");
+      this.setState({ checked: false });
+    }
   }
 }
 
