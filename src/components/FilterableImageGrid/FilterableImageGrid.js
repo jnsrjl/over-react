08611d788dbbb4694f38261b1f0a4fe40367ce2e
jnsrjl <jnsrjl@gmail.com>
2017-03-29 22:50:10 +0000

    Define state and data binding

--- src/components/FilterableImageGrid/FilterableImageGrid.js
+++ src/components/FilterableImageGrid/FilterableImageGrid.js
@@ -29,35 +29,21 @@ const PHOTOS = [
   { path: roll, name: "roll", sizeKB: 134 },
 ];
 
-class ImageGrid extends React.Component {
-  render() {
-    return (
-      <div style={styles.root}>
-        <GridList
-          cols={4}
-          style={styles.gridList}
-        >
-          <Subheader>Images</Subheader>
-          {this.props.photos.map((photo, id) => (
-            <GridTile
-              key={id}
-              title={photo.name}
-              subtitle={<span>by <b>dude</b></span>}
-            >
-              <img src={photo.path} alt="" />
-            </GridTile>
-          ))}
-        </GridList>
-      </div>
-    );
-  }
-}
-
 class Filter extends React.Component {
+
+  handleFilterChange = (e) => {
+    this.props.onFilterChange(e.target.value);
+  }
+
   render() {
     return (
       <div>
-        <RadioButtonGroup name="filter" defaultSelected="name_asc">
+        <Subheader>Active filter : { this.props.filterValue }</Subheader>
+        <RadioButtonGroup
+          name="filter"
+          defaultSelected="name_asc"
+          onChange={ this.handleFilterChange }
+        >
           <RadioButton
             value="name_asc"
             label="File Name Ascending"
@@ -80,13 +66,56 @@ class Filter extends React.Component {
   }
 }
 
+class ImageGrid extends React.Component {
+  render() {
+    return (
+      <div style={styles.root}>
+        <GridList
+          cols={4}
+          style={styles.gridList}
+        >
+          <Subheader>Images</Subheader>
+          {this.props.photos.map((photo, id) => (
+            <GridTile
+              key={id}
+              title={photo.name}
+              subtitle={<span>size <b>{photo.sizeKB}KB</b></span>}
+            >
+              <img src={photo.path} alt="" />
+            </GridTile>
+          ))}
+        </GridList>
+      </div>
+    );
+  }
+}
+
 class FilterableImageGrid extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterValue: "name_asc",
+    };
+  }
+
+  handleFilterChange = (filterValue) => {
+    this.setState({
+      filterValue: filterValue
+    });
+  }
 
   render() {
     return (
       <div>
-        <Filter />
-        <ImageGrid photos={PHOTOS}/>
+        <Filter
+          filterValue={ this.state.filterValue }
+          onFilterChange={ this.handleFilterChange }
+        />
+        <ImageGrid
+          photos={ PHOTOS }
+          filterValue={ this.state.filterValue }
+        />
       </div>
     );
   }
