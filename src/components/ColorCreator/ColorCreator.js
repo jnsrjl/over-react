634852026e6e1d575a6caa927f507960d6eebc79
jnsrjl <jnsrjl@gmail.com>
2017-04-04 12:30:11 +0000

    Finish static ColorCreator

--- src/components/ColorCreator/ColorCreator.js
+++ src/components/ColorCreator/ColorCreator.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 // MUI components
-import FlatButton from 'material-ui/FlatButton';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 import Slider from 'material-ui/Slider';
 import RaisedButton from 'material-ui/RaisedButton';
-import GridList from 'material-ui/GridList';
+import {GridList, GridTile} from 'material-ui/GridList';
+import Paper from 'material-ui/Paper';
 
 const styles = {
   colorCreator: {
@@ -14,20 +14,43 @@ const styles = {
     padding: 20,
   },
   gridList: {
-    height: 400,
     margin: '20px auto',
     border: '1px solid black',
-  }
+    overflowY: 'auto',
+  },
 };
+
+const COLORS = [
+  { name: "moss green", rgb: "rgb(255,0,0)" },
+  { name: "fuchia", rgb: "rgb(255,0,0)" },
+  { name: "salmon", rgb: "rgb(255,0,0)" },
+  { name: "powder blue", rgb: "rgb(255,0,0)" },
+  { name: "turquoise", rgb: "rgb(255,0,0)" },
+  { name: "aquamarine", rgb: "rgb(0,0,255)" },
+];
+
+const COMBOS= [
+  { name: "salmon", rgb: "rgb(250,128,114)" },
+  { name: "salmon", rgb: "rgb(250,128,114)" },
+  { name: "salmon", rgb: "rgb(250,128,114)" },
+  { name: "salmon", rgb: "rgb(250,128,114)" },
+  { name: "salmon", rgb: "rgb(250,128,114)" },
+  { name: "salmon", rgb: "rgb(250,128,114)" },
+  { name: "salmon", rgb: "rgb(250,128,114)" },
+  { name: "salmon", rgb: "rgb(250,128,114)" },
+  { name: "salmon", rgb: "rgb(250,128,114)" },
+  { name: "salmon", rgb: "rgb(250,128,114)" },
+  { name: "salmon", rgb: "rgb(250,128,114)" },
+];
 
 class UndoRedo extends React.Component {
   render() {
     return (
       <div>
-        <FlatButton
+        <RaisedButton
           label="Undo"
         />
-        <FlatButton
+        <RaisedButton
           label="Redo"
         />
       </div>
@@ -36,23 +59,54 @@ class UndoRedo extends React.Component {
 }
 
 class ColorEditor extends React.Component {
+
   render() {
     return (
       <div>
         <SelectField
           floatingLabelText="Color Name"
+          value={0}
+          onChange={null}
         >
-          <MenuItem primaryText="Purple" />
-          <MenuItem primaryText="Yellow" />
-          <MenuItem primaryText="Orange" />
-          <MenuItem primaryText="Cyan" />
+          { this.props.colors.map((item, id) => {
+            return <MenuItem
+              key={ "color_" + id }
+              value={ id }
+              primaryText={ item.name }
+            />;
+          })}
         </SelectField>
         <p>Red</p>
-        <Slider />
+        <Slider
+          name="red"
+          min={0}
+          max={255}
+          step={1}
+        />
         <p>Green</p>
-        <Slider />
+        <Slider
+          name="green"
+          min={0}
+          max={255}
+          step={1}
+        />
         <p>Blue</p>
-        <Slider />
+        <Slider
+          name="blue"
+          min={0}
+          max={255}
+          step={1}
+        />
+        <Paper
+          style={{
+            height: 100,
+            width: 100,
+            margin: 15,
+            backgroundColor: "rgb(250,128,114)",
+            textAlign: 'center',
+          }}
+          circle={true}
+        />
         <RaisedButton
           label="Add"
           primary={true}
@@ -65,19 +119,32 @@ class ColorEditor extends React.Component {
 class ColorList extends React.Component {
   render() {
     return (
-      <GridList style={ styles.gridList } >
+      <GridList
+        style={ styles.gridList }
+        cols={6}
+      >
+        { this.props.combos.map((item, id) => (
+          <GridTile
+            key={"tile_" + id}
+            title={item.name}
+            style={{
+              backgroundColor: item.rgb,
+            }}
+          />
+        ))}
       </GridList>
     );
   }
 }
 
 class ColorCreator extends React.Component {
+
   render() {
     return (
       <div style={ styles.colorCreator }>
         <UndoRedo />
-        <ColorEditor />
-        <ColorList />
+        <ColorEditor colors={COLORS} />
+        <ColorList combos={COMBOS} />
       </div>
     );
   }
